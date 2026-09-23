7dae1c30e45bb88b3ec01edf62d2a81d17e1e3cf
DhikaPutra <dhikputra.management@gmail.com>
2026-09-23 22:35:11 +0700

    Update koneksi.php
    Added comments to clarify database connection settings.

--- htdocs/latihan_db/koneksi.php
+++ htdocs/latihan_db/koneksi.php
@@ -1,8 +1,8 @@
 <?php 
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $db = "db_rpl";
+    $host = "localhost"; ## default settings
+    $user = "root"; ## default settings
+    $pass = ""; ## add password if MySQL uses password
+    $db = "db_rpl"; ## type according to the database name you created previously
 
     $koneksi = mysqli_connect($host, $user, $pass, $db);
 ?>
