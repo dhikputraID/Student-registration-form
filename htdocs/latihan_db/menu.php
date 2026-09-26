24b0ae509ed4eb1c06b4f991c5e45f5b58f25c26
DhikaPutra <dhikputra.management@gmail.com>
2026-09-26 09:53:25 +0700

    Delete htdocs/latihan_db directory

--- htdocs/latihan_db/menu.php
+++
@@ -1,7 +0,0 @@
-<hr>
-<nav>
-    <a href="dashboard.php">Dashboard</a> | 
-    <a href="tampil_mapel.php">Data Mata Pelajaran</a> | 
-    <a href="logout.php" onclick="return confirm('Yakin ingin keluar?')">Logout</a>
-</nav>
-<hr>
